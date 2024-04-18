1cb06498349fee2b68d9485163fc3258f1291f5b
GowthamRaj T <150216248+Gowthamraj-07@users.noreply.github.com>
2024-04-18 21:08:33 +0530

    Add files via upload

---
+++ React/src/Aboutus.js
@@ -0,0 +1,12 @@
+import Aboutus from "./components/AboutUsMain"
+import Footer from "./components/Footer"
+export default function Aboutuspage()
+
+{
+    return(
+        <>
+        <Aboutus/>
+        <Footer/>
+        </>
+    )
+}
